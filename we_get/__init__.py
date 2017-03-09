635822db1194b712860366f26afe632d7bcce289
Levi Sabah <0xl3vi@gmail.com>
2017-03-09 15:25:33 +0000

    Added __init__.py

---
+++ we_get/__init__.py
@@ -0,0 +1,17 @@
+"""
+Copyright (c) 2016 we-get developers (https://github.com/0xl3vi/we-get/)
+See the file 'LICENSE' for copying permission
+"""
+
+import sys
+from we_get.core.we_get import WG
+from we_get.core.utils import msg_error
+from we_get.core.utils import msg_err_trace
+
+def main():
+  we_get = WG()
+  we_get.parse_arguments()
+  try:
+    we_get.start() 
+  except (EOFError, KeyboardInterrupt):
+    msg_error("[KeyboardInterrupt]", True)
---
+++ we_get/core/__init__.py
@@ -0,0 +1,6 @@
+"""
+Copyright (c) 2016 we-get developers (https://github.com/0xl3vi/we-get/)
+See the file 'LICENSE' for copying.
+"""
+pass
+

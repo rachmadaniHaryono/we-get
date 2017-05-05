88ef1975fac1483db0d2d7db9328bbef526ec4c7
Hendrikto <hendrik.to@gmail.com>
2017-05-05 16:46:19 +0200

    Fix indentation.

--- setup.py
+++ setup.py
@@ -8,17 +8,17 @@ from setuptools import setup, find_packages
 version = '0.1'
 
 setup(
-  name='we-get',
-  version=version,
-  description='Search torrents from the command-line',
-  author='Levi Sabah',
-  author_email='0xl3vi@gmail.com',
-  license='MIT',
-  keywords=['command line', 'torrent'],
-  url='https://github.com/0xl3vi/we-get',
-  packages=find_packages(),
-  install_requires=['docopt', 'prompt_toolkit', 'colorama'],
-  include_package_data=True,
-  package_data={'we_get': ['txt/useragents.txt']},
-  entry_points={'console_scripts': ['we-get=we_get:main']}
+    name='we-get',
+    version=version,
+    description='Search torrents from the command-line',
+    author='Levi Sabah',
+    author_email='0xl3vi@gmail.com',
+    license='MIT',
+    keywords=['command line', 'torrent'],
+    url='https://github.com/0xl3vi/we-get',
+    packages=find_packages(),
+    install_requires=['docopt', 'prompt_toolkit', 'colorama'],
+    include_package_data=True,
+    package_data={'we_get': ['txt/useragents.txt']},
+    entry_points={'console_scripts': ['we-get=we_get:main']}
 )

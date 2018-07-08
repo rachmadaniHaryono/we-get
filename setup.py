1774a573e7c81c5c3d69aceb1ddea03f6e46fb93
rachmadaniHaryono <foreturiga@gmail.com>
2018-07-08 20:00:24 +0800

    chg: doc: year 2018

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2016 we-get developers (https://github.com/rachmadaniHaryono/we-get/)
+Copyright (c) 2016-2018 we-get developers (https://github.com/rachmadaniHaryono/we-get/)
 See the file 'LICENSE' for copying permission
 """
 

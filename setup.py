60d5c960faae22674dd8eb0e67b4ba9bf87c3236
rachmadaniHaryono <foreturiga@gmail.com>
2018-07-08 19:32:15 +0800

    chg: dev: set minimum version for prompt_toolkit

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     keywords=['command line', 'torrent'],
     url='https://github.com/rachmadaniHaryono/we-get',
     packages=find_packages(),
-    install_requires=['docopt', 'prompt_toolkit', 'colorama'],
+    install_requires=['docopt', 'prompt_toolkit>=2.0.3', 'colorama'],
     include_package_data=True,
     package_data={'we_get': ['txt/useragents.txt']},
     entry_points={'console_scripts': ['we-get=we_get:main']}

0e594a8a987bf31f4e712bda373ce68066b84d3f
rachmadaniHaryono <foreturiga@gmail.com>
2020-12-30 12:26:56 +0800

    new: dev: aiohttp

--- setup.py
+++ setup.py
@@ -5,28 +5,31 @@ See the file 'LICENSE' for copying permission
 
 
 from setuptools import setup, find_packages
-version = '1.1.2'
+
+version = "1.1.2"
 
 setup(
-    name='we-get',
+    name="we-get",
     version=version,
-    description='Search torrents from the command-line',
-    author='Levi Sabah',
-    author_email='0xl3vi@gmail.com',
-    license='MIT',
-    keywords=['command line', 'torrent'],
-    url='https://github.com/rachmadaniHaryono/we-get',
+    description="Search torrents from the command-line",
+    author="Levi Sabah",
+    author_email="0xl3vi@gmail.com",
+    license="MIT",
+    keywords=["command line", "torrent"],
+    url="https://github.com/rachmadaniHaryono/we-get",
     packages=find_packages(),
     install_requires=[
-        'colorama',
-        'docopt',
-        'beautifulsoup4',
-        'prompt-toolkit>=3.0.5',
-        'Pygments>=2.6.1',
+        "aiohttp>=3.7.3",
+        "beautifulsoup4",
+        "colorama",
+        "docopt",
+        "prompt-toolkit>=3.0.5",
+        "Pygments>=2.6.1",
     ],
     extras_require={
-        'test':  ["pytest", "pytest-flake8", 'vcrpy'], },
+        "test": ["pytest", "pytest-flake8", "vcrpy"],
+    },
     include_package_data=True,
-    package_data={'we_get': ['txt/useragents.txt']},
-    entry_points={'console_scripts': ['we-get=we_get:main']}
+    package_data={"we_get": ["txt/useragents.txt"]},
+    entry_points={"console_scripts": ["we-get=we_get:main"]},
 )

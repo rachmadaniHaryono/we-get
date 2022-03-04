550b9c7da8872a20e6ad10771ffc3409a6b17ba7
Rachmadani Haryono <foreturiga@gmail.com>
2022-03-05 05:30:51 +0800

    feat(module): handle any error instead of non 200 response
    black and isort formatted

--- we_get/core/module.py
+++ we_get/core/module.py
@@ -3,10 +3,12 @@ Copyright (c) 2016-2022 we-get developers (https://github.com/rachmadaniHaryono/
 See the file 'LICENSE' for copying.
 """
 
-import requests
 import urllib.parse
-from we_get.core.utils import random_user_agent
 from html import unescape as html_decode
+
+import requests
+
+from we_get.core.utils import random_user_agent
 
 USER_AGENT = random_user_agent()
 
@@ -16,37 +18,36 @@ class Module(object):
         self.cursor = None
 
     def http_get_request(self, url):
-        """ http_request: create HTTP request.
-          @return: data.
+        """http_request: create HTTP request.
+        @return: data.
         """
-        headers = [('User-Agent', USER_AGENT), ("Accept", "*/*")]
-        res = requests.get(url, headers)
-        if res.status_code == 200:
+        headers = [("User-Agent", USER_AGENT), ("Accept", "*/*")]
+        res = requests.get(url, headers=dict(headers))
+        try:
             return res.text
-        else: 
-            print('Error when opening following url.\n{}'.format(url))
+        except Exception as err:
+            print("Error when opening following url: {}.\n{}".format(err, url))
             raise err
 
     def http_custom_get_request(self, url, headers):
-        """ http_custom_get_request: HTTP GET request with custom headers.
-          @return: data.
+        """http_custom_get_request: HTTP GET request with custom headers.
+        @return: data.
         """
         return requests.get(url, headers).text
 
     def magnet2name(self, link):
-        """ magnet2name: return torrent name from magnet link.
-          @magnet - link.
+        """magnet2name: return torrent name from magnet link.
+        @magnet - link.
         """
         return link.split("&")[1].split("dn=")[1]
 
     def fix_name(self, name):
-        """ fix_name: fix the torrent_name (Hello%20%20Worl+d to Hello_World).
-        """
+        """fix_name: fix the torrent_name (Hello%20%20Worl+d to Hello_World)."""
         name = html_decode(name)
         return urllib.parse.unquote(
-            name.replace('+', '.')
-                .replace('[', '')
-                .replace(']', '')
-                .replace(' ', '.')
-                .replace('\'', '')
+            name.replace("+", ".")
+            .replace("[", "")
+            .replace("]", "")
+            .replace(" ", ".")
+            .replace("'", "")
         )
